a024f816cb2a4498402785144570e10d3869ec0e
Mr Ben <bwilson668@gmail.com>
2017-07-16 22:00:33 -0400

    header update 2

--- lib/structure/header.php
+++ lib/structure/header.php
@@ -1,58 +1,7 @@
 <?php
 
-add_action( 'wp_head', 'genesis_custom_header_style' );
-/**
- * Custom header callback.
- *
- * It outputs special CSS to the document head, modifying the look of the header based on user input.
- *
- * @since 1.6.0
- *
- * @uses genesis_html() Check for HTML5 support.
- *
- * @return null Return null on if custom header not supported, user specified own callback, or no options set.
- */
-function genesis_custom_header_style() {
-
-    //* Do nothing if custom header not supported
-    if ( ! current_theme_supports( 'custom-header' ) )
-        return;
-
-    //* Do nothing if user specifies their own callback
-    if ( get_theme_support( 'custom-header', 'wp-head-callback' ) )
-        return;
-
-    $output = '';
-
-    $header_image = get_header_image();
-    $text_color   = get_header_textcolor();
-
-    //* If no options set, don't waste the output. Do nothing.
-    if ( empty( $header_image ) && ! display_header_text() && $text_color === get_theme_support( 'custom-header', 'default-text-color' ) )
-        return;
-
-    $header_selector = get_theme_support( 'custom-header', 'header-selector' );
-    $title_selector  = genesis_html5() ? '.custom-header .site-title'       : '.custom-header #title';
-    $desc_selector   = genesis_html5() ? '.custom-header .site-description' : '.custom-header #description';
-
-    //* Header selector fallback
-    if ( ! $header_selector )
-        $header_selector = genesis_html5() ? '.custom-header .site-header' : '.custom-header #header';
-
-    //* Header image CSS, if exists
-    if ( $header_image )
-        $output .= sprintf( '%s { background: url(%s) no-repeat !important; }', $header_selector, esc_url( $header_image ) );
-
-    //* Header text color CSS, if showing text
-    if ( display_header_text() && $text_color !== get_theme_support( 'custom-header', 'default-text-color' ) )
-        $output .= sprintf( '%2$s a, %2$s a:hover, %3$s { color: #%1$s !important; }', esc_html( $text_color ), esc_html( $title_selector ), esc_html( $desc_selector ) );
-
-    if ( $output )
-        printf( '<style type="text/css">%s</style>' . "\n", $output );
-
-}
-
-add_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+add_action( 'genesis_header', 'showcase_header_markup_open', 5 );
 /**
  * Echo the opening structural markup for the header.
  *
@@ -61,7 +10,7 @@ add_action( 'genesis_header', 'genesis_header_markup_open', 5 );
  * @uses genesis_markup()          Apply contextual markup.
  * @uses genesis_structural_wrap() Maybe add opening .wrap div tag with header context.
  */
-function genesis_header_markup_open() {
+function showcase_header_markup_open() {
 
     genesis_markup( array(
         'html5'   => '<header %s>',
